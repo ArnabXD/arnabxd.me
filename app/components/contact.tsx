26ef0c42301110177a33fb4f60f7673233dc8341
ArnabXD <arnabxd@proton.me>
2025-01-03 01:10:56 +0530

    feat: add contact support via telegram

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,52 +1,25 @@
 import { useEffect } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { MetaFunction } from "@remix-run/cloudflare";
 import { Form, useSubmit } from "@remix-run/react";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 import toast from "react-hot-toast";
 
 import { Input, TextArea } from "~/components/input";
 import { useDebounce } from "~/hooks/debounce";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "Contact - ArnabXD" },
-    { name: "description", content: "Contact Arnab Paryali" },
-  ];
-};
-
-const schema = z.object({
-  firstName: z
-    .string({ required_error: "First name is required" })
-    .min(2, "First name must be at least 2 characters long"),
-  lastName: z
-    .string({ required_error: "Last name is required" })
-    .min(2, "Last name must be at least 2 characters long"),
-  email: z
-    .string({ required_error: "Email is required" })
-    .email("Please enter a valid email address"),
-  subject: z
-    .string({ required_error: "Subject is required" })
-    .min(10, "Subject must be at least 10 characters long"),
-  message: z
-    .string({ required_error: "Message is required" })
-    .min(20, "Message must be at least 20 characters long")
-    .max(1000, "Message must be at most 1000 characters long"),
-});
+import { contactSchema } from "~/services/telegram";
 
 // export const action = async ({ request }: ActionFunctionArgs) => {
 //   const data = await request.formData();
 //   const _json = Object.fromEntries(data);
 
-//   const result = await schema.safeParseAsync(_json);
+//   const result = await contactSchema.safeParseAsync(_json);
 //   if (!result.success) {
 //     return json(result);
 //   }
 
 //   return json({ success: true });
 // };
-export interface ContactProps {
+interface ContactProps {
   data?: {
     success: boolean;
   };
@@ -56,11 +29,11 @@ export interface ContactProps {
 export default function Contact({ data, headerRef }: ContactProps) {
   const {
     register,
-    formState: { errors },
+    formState: { errors, isSubmitSuccessful },
     reset,
     handleSubmit,
   } = useForm({
-    resolver: zodResolver(schema),
+    resolver: zodResolver(contactSchema),
   });
   const submit = useSubmit();
 
@@ -133,11 +106,8 @@ export default function Contact({ data, headerRef }: ContactProps) {
           />
 
           <div className="sm:col-span-2 flex justify-between items-center">
-            <button
-              type="submit"
-              className="inline-block bg-primary hover:scale-105 text-bg-light dark:text-bg-dark focus-visible:ring text-sm md:text-base font-semibold text-center rounded-lg outline-none border-transparent transition duration-100 px-8 py-3"
-            >
-              Send
+            <button type="submit" className={"primary-btn"}>
+              {isSubmitSuccessful ? "..." : "Send"}
             </button>
             <span className="text-gray-500 text-sm">*Required</span>
           </div>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,6 +9,7 @@ import Blogs, { BlogData } from "~/components/blogs";
 import Contact from "~/components/contact";
 import Hero from "~/components/hero";
 import fetchLatestPosts from "~/services/blogs";
+import { sendMessage, contactSchema, Env } from "~/services/telegram";
 
 export const meta: MetaFunction = () => {
   return [
@@ -21,9 +22,24 @@ export const loader: LoaderFunction = async () => {
   return await fetchLatestPosts("ArnabXD", 3);
 };
 
-export const action = async ({ request }: ActionFunctionArgs) => {
-  console.log(request.method);
-  return json({ success: true });
+export const action = async ({ request, context }: ActionFunctionArgs) => {
+  const body = await request.formData();
+  const jsonData = contactSchema.safeParse(Object.fromEntries(body));
+  let success = false;
+
+  if (!jsonData.success) {
+    return json({ success: false });
+  }
+
+  if (jsonData.data) {
+    success = await sendMessage(context.cloudflare.env as Env, {
+      name: jsonData.data.firstName + " " + jsonData.data.lastName,
+      email: jsonData.data.email,
+      message: jsonData.data.message,
+    });
+  }
+
+  return json({ success });
 };
 
 export default function Index() {
