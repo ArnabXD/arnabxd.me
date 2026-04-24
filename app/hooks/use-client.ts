5f3c4015469cd3f7349b74bfb0e5fb0b91ee2e5c
ArnabXD <arnabxd@pm.me>
2026-04-25 00:52:02 +0530

    base astro setup

--- app/hooks/use-client.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { useIsomorphicLayoutEffect } from "../utils/isomorphic-layout-effect";
-
-export const useIsClient = () => {
-  const [isClient, setIsClient] = useState(false);
-  useIsomorphicLayoutEffect(() => setIsClient(true), []);
-  return isClient;
-};
